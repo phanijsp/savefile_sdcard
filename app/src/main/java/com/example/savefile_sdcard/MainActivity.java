2d54f6cfaac16aee9de4c0e59362a7d8081c8f82
aditya <phani.jsp@gmail.com>
2019-08-09 16:39:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/savefile_sdcard/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.savefile_sdcard;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1000);
+        }
+        File file2 = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/aditya");
+        Boolean bool=file2.mkdir();
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/aditya","one.txt");
+        try{
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write("Hello Aditya ! ".getBytes());
+            fos.close();
+            Toast.makeText(this,"Saved in"+Environment.getExternalStorageDirectory().getAbsolutePath()+"/aditya",Toast.LENGTH_SHORT).show();
+        }catch (Exception e){
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1000:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this,"Permission Granted !",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(this,"Permission Denied !",Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+}
